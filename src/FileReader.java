293a84be81caa981aa4b64e67f00543516090bbc
Kayan Laswell <kcclaswell@gmail.com>
2024-10-09 15:21:46 -0400

    File reader now returns file contents

--- src/FileReader.java
+++ src/FileReader.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 
 public class FileReader {
 
-    public void getFileData(String filePath) {
+    public String getFileData(String filePath) {
+        StringBuilder fileContent = new StringBuilder();
         try {
             File assemblyCode = new File(filePath);
             Scanner scanner = new Scanner(assemblyCode);
             while (scanner.hasNextLine()) {
                 String data = scanner.nextLine();
-                System.out.println(data);
+                fileContent.append(data).append("\n");
             }
             scanner.close();
         }catch (FileNotFoundException e){
             System.out.println("An error");
             e.printStackTrace();
         }
+        return fileContent.toString();
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
        FileReader fileReader = new FileReader();
-       fileReader.getFileData("C:/Users/kccla/CS 230/pepasm/src/file.pep");
+       String fileData = fileReader.getFileData("C:/Users/kccla/CS 230/pepasm/src/file.pep");
+       System.out.println(fileData);
 
     }
 }
