3d0578908e3d565c6387bbe25e60f30eb7eac54f
Kayan Laswell <kcclaswell@gmail.com>
2024-10-10 10:47:03 -0400

    main does not branch

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Assembler assembler = new Assembler();
         String filePath = "C:/Users/kccla/CS 230/pepasm/src/file.pep";
         assembler.assembleFile(filePath);
